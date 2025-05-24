287a89bd83fc6a699161f2c5c32c31b2e28d378e
GD0330 <g_dtodorov30122003@abv.bg>
2025-05-24 12:33:00 +0300

    Exp

--- src/main/java/com/example/myproject/MainController.java
+++ src/main/java/com/example/myproject/MainController.java
@@ -1,14 +1,28 @@
 package com.example.myproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick() throws IOException {
         welcomeText.setText("Welcome to JavaFX Application!");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Stage stage = new Stage();
+        stage.setTitle("Dashboard");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
