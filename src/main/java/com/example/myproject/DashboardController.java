1296a6a79730c97e720dd633f395c0c0ad7f02b8
GD0330 <g_dtodorov30122003@abv.bg>
2025-05-24 21:42:59 +0300

    Exp

--- src/main/java/com/example/myproject/DashboardController.java
+++ src/main/java/com/example/myproject/DashboardController.java
@@ -1,7 +1,14 @@
 package com.example.myproject;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DashboardController {
 
@@ -9,8 +16,25 @@ public class DashboardController {
     private Label label;
 
     @FXML
+    private Button button;
+
+    @FXML
     public void initialize() {
         label.setText("Hello");
-        System.out.println("DashboardController initialized");
+        System.out.println("Close");
+    }
+
+    @FXML
+    private void onCloseButtonClick() throws IOException {
+        Stage Dashboard = (Stage) button.getScene().getWindow();
+        Dashboard.close();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Main.fxml"));
+
+        Stage stage = new Stage();
+        Scene scene1 = new Scene(fxmlLoader.load(),400,180);
+        stage.setTitle("Dashboard");
+        stage.setScene(scene1);
+        stage.show();
     }
 }
--- src/main/java/com/example/myproject/MainController.java
+++ src/main/java/com/example/myproject/MainController.java
@@ -4,25 +4,51 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() throws IOException {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private Label passwordLabel;
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
-        Parent root = fxmlLoader.load();
+    @FXML
+    public void initialize() {
+        System.out.println("Open");
+    }
+    @FXML
+    protected void onEnterButtonClick() throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
 
-        Stage stage = new Stage();
-        stage.setTitle("Dashboard");
-        stage.setScene(new Scene(root));
-        stage.show();
+        if (username.equals("GD")&&password.equals("123four")) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
+
+            Stage stage = new Stage();
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.setTitle("Dashboard");
+            stage.show();
+
+            Stage Main = (Stage) button.getScene().getWindow();
+            Main.close();
+        } else {
+            welcomeLabel.setText("Wrong username or password!");
+        }
     }
 }
